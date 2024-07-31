03aa83d0c8e91dd14b302e2b4e4821d28acb1c36
vibhaw raj <vibhawraj494@gmail.com>
2024-07-31 13:08:21 +0530

    [js]:practice

---
+++ javascriptPractice/index.js
@@ -0,0 +1,13 @@
+// let text="Hello world"
+// console.log(text.length);
+// console.log(text.charAt(0));
+
+// let part=text.slice(7);
+// let part=text.slice(-2)
+
+// console.log(part);
+// let text=5;
+// let padded = text.(4,"x")
+// console.log(padded);
+
+
