bbc752bc18434a1f864a645cc6a42232c7d273e4
vibhaw raj <vibhawraj494@gmail.com>
2024-09-17 14:53:18 +0530

    ..

---
+++ stack&queue/nextGreaterElemt2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int>st;
+        vector<int>ans;
+        for(int i=nums.size()-1;i>=0;i--){
+            st.push(nums[i]);
+        }
+        for(int i=nums.size()-1;i>=0;i--){
+            while(!st.empty()&&st.top()<=nums[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                ans.push_back(-1);
+            }
+
+            else{
+                ans.push_back(st.top());
+            }
+st.push(nums[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
